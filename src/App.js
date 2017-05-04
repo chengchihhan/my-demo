ddbc87e6c48629db983d3748af00696587cd3a1c
chengchihhan <haneric1986@gmail.com>
2017-05-04 15:12:44 +0800

    add books data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    const BOOKS = [
+      {category: 'Comics & Graphic Novels', price: '$9.99', stocked: true, name: 'Guardians of the Galaxy', popular: true},
+      {category: 'Comics & Graphic Novels', price: '$9.99', stocked: true, name: 'Dragon Ball', popular: true},
+      {category: 'Fantasy Action & Adventure', price: '$29.99', stocked: false, name: 'Harry Potter And The Order Of The Phoenix', popular: false},
+      {category: 'Fantasy Action & Adventure', price: '$9.99', stocked: true, name: 'Harry Potter and the Half-Blood Prince', popular: false},
+      {category: 'Science Fiction Adventures', price: '$39.99', stocked: false, name: 'Phasma (Star Wars)', popular: true},
+      {category: 'Science Fiction Adventures', price: '$19.99', stocked: true, name: 'Battlefront II: Inferno Squad (Star Wars)', popular: true}
+    ];
     return (
       <div className="App">
       </div>
