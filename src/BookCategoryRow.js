5013caa2642307fc9e53eab06fa0f01e7750ec5e
chengchihhan <haneric1986@gmail.com>
2017-05-04 17:48:27 +0800

    add colspan props

--- src/BookCategoryRow.js
+++ src/BookCategoryRow.js
@@ -3,7 +3,7 @@ import React from 'react';
 class BookCategoryRow extends React.Component {
   render() {
     return (
-      <tr><th>{this.props.category}</th></tr>
+      <tr><th colSpan="4">{this.props.category}</th></tr>
     )
   }
 }
