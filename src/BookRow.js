9de551950770829e3deea85db9cbc9a0a6dcf166
chengchihhan <haneric1986@gmail.com>
2017-05-04 15:47:26 +0800

    modify book row

--- src/BookRow.js
+++ src/BookRow.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 class BookRow extends React.component {
   render() {
+    let popular = this.props.book.popular ?
+      <span style={{color: red}}>Hot</span> :
+      <span style={{color: blue}}>New</span>
     return (
-      <div></div>
+      <tr>
+        <td>{this.props.book.name}</td>
+        <td>{this.props.book.price}</td>
+        <td>{popular}</td>
+      </tr>
     )
   }
 }
