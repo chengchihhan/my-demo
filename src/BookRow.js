ad16a0d8e13d6bd5b5cbaecae83e5b8d5838d436
chengchihhan <haneric1986@gmail.com>
2017-05-04 17:24:50 +0800

    rename variable popular -> status

--- src/BookRow.js
+++ src/BookRow.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class BookRow extends React.Component {
   render() {
-    let popular = this.props.book.popular ?
-      <span style={{color: 'red'}}>Hot</span> :
-      <span style={{color: 'blue'}}>New</span>
+    let status = this.props.book.popular ?
+      <span style={{color: 'red'}}>Hot!</span> :
+      <span style={{color: 'blue'}}>New!</span>;
     return (
       <tr>
         <td>{this.props.book.name}</td>
         <td>{this.props.book.price}</td>
-        <td>{popular}</td>
+        <td>{status}</td>
       </tr>
     )
   }
