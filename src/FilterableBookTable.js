a57fc31491c4e39e654d4b5417995a97bd8c7df7
chengchihhan <haneric1986@gmail.com>
2017-05-04 16:56:35 +0800

    modify filterable book table

--- src/FilterableBookTable.js
+++ src/FilterableBookTable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import BookTable from './BookTable';
 
-class FilterableBookTable extends React.component {
+class FilterableBookTable extends React.Component {
   render() {
     return (
       <div>
