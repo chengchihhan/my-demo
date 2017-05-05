b07f00e57645596a660cc8dbad62d48688c06193
chengchihhan <haneric1986@gmail.com>
2017-05-05 11:02:17 +0800

    add constructor and init state

--- src/FilterableBookTable.js
+++ src/FilterableBookTable.js
@@ -3,11 +3,25 @@ import SearchBar from './SearchBar';
 import BookTable from './BookTable';
 
 class FilterableBookTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterInputText: '',
+      bestSellerOnly: false
+    }
+  }
   render() {
     return (
       <div>
-        <SearchBar />
-        <BookTable books={this.props.books} />
+        <SearchBar
+          filterInputText={this.state.filterInputText}
+          bestSellerOnly={this.state.bestSellerOnly}
+        />
+        <BookTable
+          books={this.props.books}
+          filterInputText={this.state.filterInputText}
+          bestSellerOnly={this.state.bestSellerOnly}
+        />
       </div>
     )
   }
