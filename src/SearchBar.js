2a1fd797cd080240e175fb7a9a67a60456b87a84
chengchihhan <haneric1986@gmail.com>
2017-05-05 11:58:02 +0800

    add event listener

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+    this.handleBestSellerOnlyInputChange = this.handleBestSellerOnlyInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+  handleBestSellerOnlyInputChange(e) {
+    this.props.onBestSellerOnlyInput(e.target.value);
+  }
   render() {
     return (
       <form>
@@ -8,11 +20,13 @@ class SearchBar extends React.Component {
           type="text"
           placeholder="Search..."
           value={this.props.filterText}
+          onChange={this.handleFilterTextInputChange}
         />
         <p>
           <input
             type="checkbox"
             value={this.props.bestSellerOnly}
+            onChange={this.props.handleBestSellerOnlyInputChange}
           />
           {' '}
           Only show Best Sellers
