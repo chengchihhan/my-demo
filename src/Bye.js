11e92a976beacf0e6cd7eb999ad5822054ebd1e6
chengchihhan <haneric1986@gmail.com>
2017-05-11 15:13:10 +0800

    add component named Bye

---
+++ src/Bye.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Bye extends Component {
+
+  render() {
+    return (
+      <p>
+        Bye {this.props.name}!
+      </p>
+    );
+  }
+}
+
+export default Bye;
