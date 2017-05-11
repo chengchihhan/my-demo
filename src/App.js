79e8655f7b7aa35908f1cdb603dde1dec41503ef
chengchihhan <haneric1986@gmail.com>
2017-05-11 15:14:11 +0800

    insert HOC(Bye)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import HOC from './HOC';
 import Hello from './Hello';
+import Bye from './Bye';
 
 class App extends Component {
   render() {
 
     const Wrap = HOC(Hello);
+    const Wrap2 = HOC(Bye);
 
     return (
       <div>
         <Wrap />
+        <Wrap2 />
       </div>
     );
   }
